e2b0d386d8bca60b1a99d25c27625403cd88ede4
falkenstein <josefhertl@gmail.com>
2019-05-05 13:34:28 +0200

    Initial commit

---
+++ src/main/kotlin/com/roihunter/facehunter/Application.kt
@@ -0,0 +1,18 @@
+package main.kotlin.com.roihunter.facehunter
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+
+/**
+ * The main app file.
+ */
+@SpringBootApplication
+@EnableScheduling
+open class Application {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
---
+++ src/main/kotlin/com/roihunter/facehunter/flow/NewGuessFlow.kt
@@ -0,0 +1,17 @@
+package main.kotlin.com.roihunter.facehunter.flow
+
+import org.springframework.stereotype.Service
+
+/**
+ * Posts a new guess to given user.
+ */
+@Service
+class NewGuessFlow {
+
+    /**
+     * Sends a new guess to the specified user.
+     */
+    fun newGuess() {
+
+    }
+}
