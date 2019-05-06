58379111c0750a9282c0574ad81885a54390502d
falkenstein <josefhertl@gmail.com>
2019-05-06 16:59:47 +0200

    Fix payload not parsed properly

--- src/main/kotlin/com/roihunter/facehunter/controller/InteractiveController.kt
+++ src/main/kotlin/com/roihunter/facehunter/controller/InteractiveController.kt
@@ -5,15 +5,12 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.roihunter.facehunter.dto.slack.interactive.PayloadDto
 import com.roihunter.facehunter.flow.EvaluateGuessFlow
 import com.roihunter.facehunter.flow.NewGuessFlow
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import java.lang.IllegalStateException
 
 @RestController
 class InteractiveController(
@@ -29,17 +26,18 @@ class InteractiveController(
     )
     @ResponseStatus(HttpStatus.OK)
     fun slackInteractiveResponse(
-            @RequestParam payload: PayloadDto
+            @RequestParam payload: String
     ) {
-        val pickedAction = payload.actions.first()
+        val payloadDto: PayloadDto = mapper.readValue(payload)
+        val pickedAction = payloadDto.actions.first()
         if (pickedAction.value != "next") { // Means we're evaluating a guess.
             val splitParts = pickedAction.value?.split("..") ?: throw IllegalStateException("Missing clicked button value!")
             val correct = splitParts[0]
             val correctName = splitParts[1]
             val position = splitParts[2]
-            evaluateGuessFlow.evaluateGuess(correct == "correct", correctName, position, payload.user.id, payload.team.id)
+            evaluateGuessFlow.evaluateGuess(correct == "correct", correctName, position, payloadDto.user.id, payloadDto.team.id)
         } else {
-            newGuessFlow.newGuess(payload.user.id, payload.team.id)
+            newGuessFlow.newGuess(payloadDto.user.id, payloadDto.team.id)
         }
     }
 }
