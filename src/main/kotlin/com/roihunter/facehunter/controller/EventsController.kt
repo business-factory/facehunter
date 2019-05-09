80eafbd059ede8d75f344fc333f16804b50a5f91
falkenstein <josefhertl@gmail.com>
2019-05-09 14:18:30 +0200

    React to help message event with help message

--- src/main/kotlin/com/roihunter/facehunter/controller/EventsController.kt
+++ src/main/kotlin/com/roihunter/facehunter/controller/EventsController.kt
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.roihunter.facehunter.dto.slack.event.ChallengeResponseDto
 import com.roihunter.facehunter.dto.slack.event.EventDto
+import com.roihunter.facehunter.flow.SendHelpInfoFlow
 import com.roihunter.facehunter.manager.SlackManager
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class EventsController(
         private val slackManager: SlackManager,
-        private val mapper: ObjectMapper
+        private val mapper: ObjectMapper,
+        private val sendHelpInfoFlow: SendHelpInfoFlow
 ) {
 
     @PostMapping(value = ["/event"])
@@ -30,6 +31,9 @@ class EventsController(
         val event: EventDto = mapper.readValue(body)
         if (event.type == "url_verification") {
             return ChallengeResponseDto(event.challenge ?: throw IllegalStateException("Challenge request without challenge value. $event"))
+        } else if (event.type == "event_callback" && event.text?.contains("help", ignoreCase = true) == true && event.user != null && event.team != null) {
+            sendHelpInfoFlow.sendHelpInfo(event.user, event.team)
+            return null
         } else {
             return null
         }
--- src/main/kotlin/com/roihunter/facehunter/dto/slack/event/EventDto.kt
+++ src/main/kotlin/com/roihunter/facehunter/dto/slack/event/EventDto.kt
@@ -3,10 +3,14 @@ package com.roihunter.facehunter.dto.slack.event
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class EventDto(
-        @JsonProperty("token")
-        val token: String?,
         @JsonProperty("challenge")
         val challenge: String?,
         @JsonProperty("type")
-        val type: String?
+        val type: String?,
+        @JsonProperty("user")
+        val user: String?,
+        @JsonProperty("text")
+        val text: String?,
+        @JsonProperty("team")
+        val team: String?
 )
